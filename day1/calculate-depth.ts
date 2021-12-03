c04a00d460893f0dd5e7cd3315c466931aba7c32
Taher Dhilawala <taher.dhilawala@klarna.com>
2021-12-03 17:10:38 +0100

    Advent of code day 1

---
+++ day1/calculate-depth.ts
@@ -0,0 +1,31 @@
+// import fetch, { Response } from 'node-fetch'
+import { input } from './input'
+
+const calculateIncreaseDepthCount = (input: Array<number>) => {
+  let increasedDepthCount = 0
+  for (let i = 1; i < input.length; i++) {
+    increasedDepthCount += input[i] > input[i - 1] ? 1 : 0
+  }
+  return increasedDepthCount
+}
+
+const calculateSlidingWindowIncreaseDepthCount = (input: Array<number>) => {
+  let previousWindowSum
+  let increasedDepthCount = 0
+  const windowSize = 3
+  for (let i = 0; i <= input.length - windowSize; i++) {
+    const window = input.slice(i, i + windowSize)
+    const currentWindowSum = window.reduce((sum: number, n: number) => sum + n, 0)
+
+    increasedDepthCount += !!previousWindowSum && currentWindowSum > previousWindowSum ? 1 : 0
+    previousWindowSum = currentWindowSum
+  }
+  return increasedDepthCount
+}
+
+const run = () => {
+  // console.log(calculateIncreaseDepthCount(input))
+  console.log(calculateSlidingWindowIncreaseDepthCount(input))
+}
+
+run()
